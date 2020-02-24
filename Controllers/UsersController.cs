c0b0fc85581e583722a9039d5700109c42e5c704
Iana <himishoichi@gmail.com>
2020-02-24 13:36:35 +0300

    Updating resources

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -4,12 +4,14 @@ using DatingApp.API.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace DatingApp.API.Controllers
 {
     [Authorize]
     [Route("api/[controller]")]
+    [ApiController]
     public class UsersController: ControllerBase
     {
         private readonly IDatingRepository _repo;
@@ -38,6 +40,18 @@ namespace DatingApp.API.Controllers
             return Ok(userToReturn);
         }
 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto) 
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
 
+            var userFromRepo = await _repo.GetUser(id);
+            _mapper.Map(userForUpdateDto, userFromRepo);
+            if (await _repo.SaveAll())
+                return NoContent();
+
+            throw new System.Exception($"Updating user {id} failed on save");
+        }
     }
 }
